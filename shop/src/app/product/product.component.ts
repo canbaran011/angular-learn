80870e0aa58cd82089373b86275b461be10a020f
Can Baran Gündüz <canbaran011@gmail.com>
2021-07-22 20:21:50 +0300

    59HttpPostEnds

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import {AlertifyService} from '../services/alertify.service';
 import { ProductService } from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -13,7 +14,8 @@ export class ProductComponent implements OnInit {
 
   constructor(
     private alertifyService:AlertifyService,
-    private productService:ProductService
+    private productService:ProductService,
+    private activatedRoute:ActivatedRoute
     ) { }
   title = "Product List"
   filterText=""
@@ -21,9 +23,14 @@ export class ProductComponent implements OnInit {
   
 
     ngOnInit(): void {
-      this.productService.getProducts().subscribe(data => {
-        this.products = data
+
+      this.activatedRoute.params.subscribe(params=>{
+        this.productService.getProducts(params["categoryId"]).subscribe(data => {
+          this.products = data
+        });
       });
+
+
 
   }
     addToCart(product:Product):void{
