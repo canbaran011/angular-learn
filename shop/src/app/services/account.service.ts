e183aecb20560f7b326a58f40ccb77279063bb89
Can Baran Gündüz <canbaran011@gmail.com>
2021-07-23 15:51:44 +0300

    66LoginOutEnds

---
+++ shop/src/app/services/account.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { User } from '../login/user';
+
+@Injectable()
+export class AccountService {
+
+  constructor() { }
+  loggedIn = false;
+
+  login(user:User):boolean{
+
+    if(user.userName=="can" && user.password=="1234"){
+    
+    this.loggedIn=true;
+    //local storage da login olup olmadigi tutulacak
+    localStorage.setItem("isLogged",user.userName);
+    return true;
+    }
+    return false;
+  }
+
+  isLoggedIn(){
+    return this.loggedIn;
+  }
+  
+  logOut(){
+    localStorage.removeItem("isLogged");
+    this.loggedIn = false;
+  }
+}
